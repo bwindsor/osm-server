8b5beaeb57f7df20f70e223bfa5ac56e4987705d
Ben Windsor <ben.windsor@cambridgeconsultants.com>
2018-01-26 17:30:03 +0000

    Make sleep time slightly shorter

--- initialise_tile_server.bat
+++ initialise_tile_server.bat
@@ -1,6 +1,6 @@
-REM Create new containers, making sure we build, and detach from them
+REM Create new containers, make sure they are build to the latest version, and detach from them
 docker-compose up --build -d
 REM Wait to make sure containers are properly up and running
-sleep 20s
+sleep 15s
 REM Restart containers
 docker-compose restart
